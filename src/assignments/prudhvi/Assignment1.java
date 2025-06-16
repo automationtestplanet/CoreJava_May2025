20936480e5fdf3968564190a0a4af9bf3dc3b3bd
RAJU CHELLE <rajuchelle@gmail.com>
2025-06-16 07:25:53 +0530

    assignments added

---
+++ src/assignments/prudhvi/Assignment1.java
@@ -0,0 +1,12 @@
+package assignments.prudhvi;
+
+public class Assignment1 {
+
+	//Write a reusable method to find  out duplicate characters count from a string
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
