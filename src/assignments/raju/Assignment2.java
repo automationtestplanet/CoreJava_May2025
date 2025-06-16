0fe891b8abc45320a7cfd14b3be0280e61bf12a4
RAJU CHELLE <rajuchelle@gmail.com>
2025-06-16 08:08:23 +0530

    assginment added to raju

---
+++ src/assignments/raju/Assignment2.java
@@ -0,0 +1,13 @@
+package assignments.raju;
+
+public class Assignment2 {
+	
+	//Write a reusable method to print unique Characters from the string
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
