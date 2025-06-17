65104ae0991ab9f1c3bbebcfe48792d053bf2507
prudhvi <prudhvirajudendukuri3301@gmail.com>
2025-06-17 15:38:11 +0530

    prudhvi Assignment1

--- src/assignments/prudhvi/Assignment1.java
+++ src/assignments/prudhvi/Assignment1.java
@@ -2,13 +2,28 @@ package assignments.prudhvi;
 
 public class Assignment1 {
 
+
+
+
+
 	//Write a reusable method to convert each character into number and make the sum of a given word.
 	// Ex: java -> 106 + 97 + 118 + 97 -> 418;
 	// ASCCI Codes of each character are ->  j->106, a-97, v-> 118, a- 97
 	
-	public static void main(String[] args) {
-		
+	
+	public static int getAsciiSum(String word) {
+        int sum = 0;
+        for (int i = 0; i < word.length(); i++) {
+            sum += word.charAt(i);
+        }
+        return sum;
+    }
 
-	}
-
+    public static void main(String[] args) {
+    	String value = "java";
+        int result = getAsciiSum(value);
+        System.out.println( "ASCII sum of" +value+ "is" +result);
+    }
 }
+
+
