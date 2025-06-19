4429fcd4eb2e314e721b1c6c24b88b2df09de7ba
RAJU CHELLE <rajuchelle@gmail.com>
2025-06-19 08:09:21 +0530

    java class10 code

---
+++ src/oops/inheritance/College.java
@@ -0,0 +1,12 @@
+package oops.inheritance;
+
+public class College {
+	public static final String COLLEGE_NAME = "MLR College";
+	public static final int COLLEGE = 12345;
+
+	public void displayCollegeDetails() {
+		System.out.println("College Name: " + COLLEGE_NAME);
+		System.out.println("College Code: " + COLLEGE);
+	}
+
+}
