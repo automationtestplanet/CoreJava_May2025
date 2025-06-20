baadfe04f67c794f1f4d88887ca77a270fa9bb70
Madhusudhana Rao Boyapati <madhuboyapati88@gmail.com>
2025-06-20 22:38:14 +0530

    Assignment1 modified by madhusudhan

--- src/assignments/madhusudhan/Assignment1.java
+++ src/assignments/madhusudhan/Assignment1.java
@@ -1,7 +1,6 @@
 package assignments.madhusudhan;
 
 public class Assignment1 {
-
 	// Write a reusable method to display Each word first letter capital in a
 	// sentence
 	// Ex: heLLo tHis iS jaVA
@@ -10,23 +9,11 @@ public class Assignment1 {
 		if (input == null || input.isEmpty()) {
 			return input;
 		}
-
-		// split the input string into words using whitespace
 		String[] words = input.split("\\s");
-
-		// create a StringBuilder
 		StringBuilder result = new StringBuilder();
-
 		for (String word : words) {
-			// capitalize the first letter of each word and append the rest of the word
-			result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase()).append(" "); // Add
-																														// a
-																														// space
-																														// between
-																														// words
+			result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase()).append(" ");
 		}
-
-		// remove the trailing space and return the capitalized string
 		return result.toString().trim();
 	}
 
@@ -38,5 +25,4 @@ public class Assignment1 {
 		System.out.println("Capitalized String: " + capitalizedString);
 
 	}
-
 }
