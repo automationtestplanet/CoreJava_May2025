8bb0f46b2c195590bc09971705e838f7f0328e2b
MadhusudhanaRaoBoyapati <madhuboyapati88@gmail.com>
2025-06-20 23:27:12 +0530

    Update Assignment1.java

--- src/assignments/madhusudhan/Assignment1.java
+++ src/assignments/madhusudhan/Assignment1.java
@@ -10,33 +10,21 @@ public class Assignment1 {
 		if (input == null || input.isEmpty()) {
 			return input;
 		}
-
-		// split the input string into words using whitespace
 		String[] words = input.split("\\s");
 
-		// create a StringBuilder
 		StringBuilder result = new StringBuilder();
 
 		for (String word : words) {
-			// capitalize the first letter of each word and append the rest of the word
-			result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase()).append(" "); // Add
-																														// a
-																														// space
-																														// between
-																														// words
+			result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase()).append(" "); 													// words
 		}
 
-		// remove the trailing space and return the capitalized string
 		return result.toString().trim();
 	}
-
 	public static void main(String[] args) {
 		String inputString = "welcome to geeksforgeeks";
 		String capitalizedString = capitalizeWords(inputString);
 
 		System.out.println("Original String: " + inputString);
 		System.out.println("Capitalized String: " + capitalizedString);
-
 	}
-
 }
