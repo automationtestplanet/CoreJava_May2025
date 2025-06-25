d07e0ddcbc9a3adb7fb0f86185ef1975b8f15dd6
prudhvi <prudhvirajudendukuri3301@gmail.com>
2025-06-25 20:53:02 +0530

    Assignment2 modified

--- src/assignments/prudhvi/Assignment2.java
+++ src/assignments/prudhvi/Assignment2.java
@@ -3,13 +3,31 @@ package assignments.prudhvi;
 public class Assignment2 {
 
 	// Write program to print occurrences of each character in a string
-	// Ex: hello  
+	// Ex: hello
 	// o/p: h-1, e-1, l-2, o-1
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void printoccurrencesofeachcharacterinastring(String str) {
 
+		String s = "hello";
+		char[] ch = s.toCharArray();
+		boolean[] b = new boolean[s.length()];
+
+		for (int i = 0; i < ch.length; i++) {
+			if (b[i])
+				continue;
+			int c = 1;
+			for (int j = i + 1; j < ch.length; j++) {
+				if (ch[i] == ch[j]) {
+					c++;
+
+					b[j] = true;
+
+				}
+			}
+			System.out.println(ch[i] + " : " + c);
+		}
 	}
 
+	public static void main(String[] args) {
+		printoccurrencesofeachcharacterinastring("hello");
+	}
 }
